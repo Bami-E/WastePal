6a4330f1a3539cc49c7358d8ab11230e8be7eca7
chizz96 <chisomnwachukwu2001@gmail.com>
2026-08-11 01:12:50 +0100

    added an eventlog

--- src/modules/eventlog/eventlog.route.js
+++ src/modules/eventlog/eventlog.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { validate } from "../../utils/validate.js";
+import { Router } from "express";
 import * as eventController from "./eventlog.controller.js";
 import { bookingCreatedSchema, bookingMatchedSchema, pickupCompletedSchema, paymentSaleSchema, pickerActivitySchema } from "./eventlog.validate.js";
 
-export const router = express.Router();
+
+const router = Router();
 
 
 router.post("/booking-created", validate(bookingCreatedSchema), eventController.bookingCreated);
@@ -12,3 +14,5 @@ router.post("/pickup-completed", validate(pickupCompletedSchema), eventControlle
 router.post("/payment-sale", validate(paymentSaleSchema), eventController.paymentSale);
 router.post("/picker-activity", validate(pickerActivitySchema), eventController.pickerActivity);
 router.get("/", eventController.list);
+
+export default router
